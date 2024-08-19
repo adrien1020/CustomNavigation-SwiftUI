0ea81fbb9b724019cc68db41eacc5fa13bf4e3b3
Adrien <adrien.surugue@gmail.com>
2024-08-19 21:00:01 +0200

    Initial Commit

---
+++ CustomNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomNavigation
+//
+//  Created by Adrien Surugue on 19/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomNavigation/CustomNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationApp.swift
+//  CustomNavigation
+//
+//  Created by Adrien Surugue on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
